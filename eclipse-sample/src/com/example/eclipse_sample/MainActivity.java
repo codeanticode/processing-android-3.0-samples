0eb3353c7cf85b9375ae643fa29a1fc34dbd8044
codeanticode <andres.colubri@gmail.com>
2016-03-20 17:56:03 -0400

    eclipse-sample project

---
+++ eclipse-sample/src/com/example/eclipse_sample/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.eclipse_sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+
+public class MainActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    FragmentManager fragmentManager = getFragmentManager();
+    Fragment fragment = new Sketch();
+    fragmentManager.beginTransaction()
+                  .replace(R.id.container, fragment)
+                  .commit();
+  }
+}
---
+++ eclipse-sample/src/com/example/eclipse_sample/Sketch.java
@@ -0,0 +1,19 @@
+package com.example.eclipse_sample;
+
+import processing.core.PApplet;
+
+public class Sketch extends PApplet {
+  public void settings() {
+    size(600, 600);
+  }
+  
+  public void setup() {
+    
+  }
+  
+  public void draw() {
+    if (mousePressed) {
+      ellipse(mouseX, mouseY, 50, 50);
+    }
+  }  
+}
