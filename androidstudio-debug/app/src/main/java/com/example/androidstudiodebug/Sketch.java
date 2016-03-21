2527639f01258bc3228cbee6d36318ffda93b9e7
codeanticode <andres.colubri@gmail.com>
2016-03-20 21:40:03 -0400

    added AndroidStudio Debug project

---
+++ androidstudio-debug/app/src/main/java/com/example/androidstudiodebug/Sketch.java
@@ -0,0 +1,19 @@
+package com.example.androidstudiodebug;
+
+import processing.core.PApplet;
+
+public class Sketch extends PApplet {
+    public void settings() {
+        size(600, 600);
+    }
+
+    public void setup() {
+      fill(250, 50, 90);
+    }
+
+    public void draw() {
+        if (mousePressed) {
+            rect(mouseX - 25, mouseY - 25, 50, 50);
+        }
+    }
+}
